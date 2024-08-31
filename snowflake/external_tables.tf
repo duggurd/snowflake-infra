ba92f6aeb5fc9aefbdee7159ce923afb751d0579
duggurd <alexanderhaugerud@gmail.com>
2024-08-31 23:59:26 +0200

    external table for minio s3 mirror

--- snowflake/external_tables.tf
+++ snowflake/external_tables.tf
@@ -1,19 +1,20 @@
-# resource "snowflake_external_table" "finn_metadata" {
-#     name = "FINN_METADATA"
-#     database = snowflake_database.prod.name
-#     schema = snowflake_schema.raw.name
-#     file_format = snowflake_file_format.gzip_parquet.name
-#     location = "@${snowflake_stage.s3_minio_mirror.database}.${snowflake_stage.s3_minio_mirror.schema}.${snowflake_stage.s3_minio_mirror.name}"
-#     column {
-#         name = "date"
-#         type = "string"
-#         as = "METADATA$FILENAME"
-#     }
-# }
+resource "snowflake_external_table" "s3_minio_mirror" {
+    name = "EXTERNAL_S3_MINIO_MIRROR"
+    database = snowflake_database.prod.name
+    schema = snowflake_schema.raw.name
+    file_format = "TYPE = PARQUET"
+    location = "@${snowflake_stage.s3_minio_mirror.database}.${snowflake_stage.s3_minio_mirror.schema}.${snowflake_stage.s3_minio_mirror.name}"
+    
+    column {
+        name = "source_file"
+        type = "string"
+        as = "METADATA$FILENAME"
+    }
+}
 
 
 resource "snowflake_file_format" "gzip_parquet" {
-    name = "gzip_parquet"
+    name = "GZIP_PARQUET"
     format_type = "PARQUET"
     compression = "AUTO"
     database = snowflake_database.prod.name
