46ebdfbffa1be41169ae4646a53fa7f06233c300
duggurd <alexanderhaugerud@gmail.com>
2024-08-31 12:34:07 +0200

    new schema security

--- snowflake/databases.tf
+++ snowflake/databases.tf
@@ -6,3 +6,8 @@ resource "snowflake_schema" "raw" {
     name = "RAW"
     database = snowflake_database.prod.name
 }
+
+resource "snowflake_schema" "security" {
+    name = "SECURITY"
+    database = snowflake_database.prod.name
+}
