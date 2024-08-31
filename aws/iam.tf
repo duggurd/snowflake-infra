151042b6ea50499820bc86b7baed57357a839f4c
duggurd <alexanderhaugerud@gmail.com>
2024-09-01 00:00:59 +0200

    snowflake trusted entity for s3 iceberg access

--- aws/iam.tf
+++ aws/iam.tf
@@ -3,6 +3,7 @@ resource "aws_iam_user" "minio_mirror" {
 
 }
 
+# Documents
 data "aws_iam_policy_document" "minio_mirror" {
     statement {
         sid = "1"
@@ -19,11 +20,75 @@ data "aws_iam_policy_document" "minio_mirror" {
     }
 }
 
+data "aws_iam_policy_document" "snowflake_s3_mirror" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:DeleteObject",
+      "s3:DeleteObjectVersion"
+    ]
+    resources = ["${aws_s3_bucket.snowflake_ingestion.arn}/*"]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation"
+    ]
+    resources = ["${aws_s3_bucket.snowflake_ingestion.arn}"]
+    condition {
+      test = "StringLike"
+      variable = "s3:prefix"
+      values = ["*"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "sf_minio_mirror_assume_role" {
+    statement {
+        effect = "Allow"
+        actions = ["sts:AssumeRole"]
+        principals {
+            type = "AWS"
+            identifiers = ["arn:aws:iam::211125613752:user/externalstages/cibxx60000"]
+        }
+        condition {
+         test = "StringEquals"
+         values = [ "iceberg_external_id" ]
+         variable = "sts:ExternalId"
+        }
+    }
+}
+
+
+
+# Policies
 resource "aws_iam_user_policy" "minio_mirror" {
   name = "minio_mirror_user_policy"
   user = aws_iam_user.minio_mirror.name
   policy = data.aws_iam_policy_document.minio_mirror.json
 }
+
+resource "aws_iam_policy" "sf_minio_mirror" {
+  policy = data.aws_iam_policy_document.snowflake_s3_mirror.json
+}
+
+
+
+# Roles
+resource "aws_iam_role" "sf_minio_mirror" {
+  name = "sf_minio_mirror"
+  assume_role_policy = data.aws_iam_policy_document.sf_minio_mirror_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "minio_sf_mirror_attachment" {
+  role = aws_iam_role.sf_minio_mirror.name
+  policy_arn = aws_iam_policy.sf_minio_mirror.arn
+}
+
 
 resource "aws_iam_access_key" "minio_mirror" {
   user = aws_iam_user.minio_mirror.name
@@ -37,3 +102,5 @@ output "minio_mirror_access_key_secret" {
 output "minio_mirror_access_key_id" {
   value = aws_iam_access_key.minio_mirror.id
 }
+
+
